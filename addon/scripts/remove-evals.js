4bec9b2609ddc74a0b0c1d20e9da8cce9daab246
Vitaliy Bavykin <vbavykin@geeksforless.net>
2018-08-13 20:40:26 +0300

    fix remove eval for production

--- addon/scripts/remove-evals.js
+++ addon/scripts/remove-evals.js
@@ -11,7 +11,7 @@ const bundles = [
     'options/options.js',
 ];
 
-const evalRegexForProduction = /;([a-z])=function\(\){return this}\(\);try{\1=\1\|\|Function\("return this"\)\(\)\|\|\(0,eval\)\("this"\)}catch\(t\){"object"==typeof window&&\(\1=window\)}/g;
+const evalRegexForProduction = /;([a-z])=function\(\){return this}\(\);try{\1=\1\|\|Function\("return this"\)\(\)\|\|\(0,eval\)\("this"\)}catch\([a-z]\){"object"==typeof window&&\(\1=window\)}/g;
 const evalRegexForDevelopment = /;\s*\/\/ This works in non-strict mode\s*([a-z])\s*=\s*\(\s*function\(\)\s*\{\s*return this;\s*}\)\(\);\s*try\s*{\s*\/\/\s*This works if eval is allowed(?:\s*|.+){1,14}/g;
 
 const removeEvals = (file) => {
