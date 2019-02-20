aa619dfd665098d2b03658bab49063a0128a1082
Drive4ik <drive4ik@gmail.com>
2019-02-20 19:48:22 +0200

    update npm packages, script evals, version

--- addon/scripts/remove-evals.js
+++ addon/scripts/remove-evals.js
@@ -11,41 +11,81 @@ const bundles = [
     'options/options.js',
 ];
 
-const evalRegexForProduction = /;([a-z])=function\(\){return this}\(\);try{\1=\1\|\|new Function\("return this"\)\(\)}catch\([a-z]\){"object"==typeof window&&\(\1=window\)}/g;
-const evalRegexForDevelopment = /;\s*\/\/ This works in non-strict mode\s*([a-z])\s*=\s*\(\s*function\(\)\s*\{\s*return this;\s*}\)\(\);\s*try\s*{\s*\/\/\s*This works if eval is allowed(?:\s*|.+){1,14}/g;
+const replaceEvalRegexp = {
+    dev: [{
+        fr: /;\s*\/\/ This works in non-strict mode\s*([a-z])\s*=\s*\(\s*function\(\)\s*\{\s*return this;\s*}\)\(\);\s*try\s*{\s*\/\/\s*This works if eval is allowed(?:\s*|.+){1,14}/g,
+        to: '=window;',
+    }, {
+        fr: 'self.Math==Math?self:Function("return this")();',
+        to: 'self.Math==Math?self:window;',
+    }, {
+        fr: 'svgContainer.innerHTML = "<svg>"',
+        to: 'svgContainer.innerText = "<svg>"',
+    }],
+    prod: [{
+        fr: /;([a-z])=function\(\){return this}\(\);try{\1=\1\|\|new Function\("return this"\)\(\)}catch\([a-z]\){"object"==typeof window&&\(\1=window\)}/g,
+        to: '=window;',
+    }, {
+        fr: 'self.Math==Math?self:Function("return this")();',
+        to: 'self.Math==Math?self:window;',
+    }, {
+        fr: 'document.createElement("div")).innerHTML="<svg>"',
+        to: 'document.createElement("div")).innerText="<svg>"',
+    }],
+};
 
-const removeEvals = (file) => {
+function removeEvals(file) {
     return new Promise((resolve, reject) => {
-        fs.readFile(file, 'utf8', (err, data) => {
+        fs.readFile(file, 'utf8', function(err, data) {
             if (err) {
                 reject(err);
                 return;
             }
 
-            data = data.replace('self.Math==Math?self:Function("return this")();', 'self.Math==Math?self:window;'); // vue-swatches plugin
+            let regexArray = process.env.IS_PRODUCTION ? replaceEvalRegexp.prod : replaceEvalRegexp.dev;
 
-            const regex = process.env.IS_PRODUCTION ? evalRegexForProduction : evalRegexForDevelopment;
+            console.info('File: ' + file);
 
-            if (!regex.test(data)) {
-                reject(`Error: no eval CSP specific code found in ${file}.`);
+            let notFound = regexArray.some(function({fr, to}) {
+                if ('string' === typeof fr) {
+                    if (!data.includes(fr)) {
+                        reject(`===> Error: no string '${fr}' found in ${file}.`);
+                        return true;
+                    }
+
+                    console.info('Replace string OK');
+                } else { // regexp
+                    if (!fr.test(data)) {
+                        reject(`===> Error: no eval CSP specific code found in ${file}.`);
+                        return true;
+                    }
+
+                    console.info('Removing eval() OK');
+                }
+
+                data = data.replace(fr, to);
+            });
+
+            if (notFound) {
                 return;
             }
-
-            console.info(`Removing eval() from ${file}`);
-
-            data = data.replace(regex, '=window;');
 
             fs.writeFile(file, data, err => err ? reject(err) : resolve());
         });
     });
-};
+}
 
-const main = () => {
-    bundles.forEach(bundle => {
-        removeEvals(path.join(BUNDLE_DIR, bundle))
-            .then(() => console.info(`Bundle ${bundle}: OK`))
-            .catch(console.error);
+function main() {
+    console.info('============= remove evals =============');
+
+    bundles.forEach(async function(bundle) {
+        try {
+            await removeEvals(path.join(BUNDLE_DIR, bundle))
+            console.info(`Bundle ${bundle}: OK`);
+        } catch (e) {
+            console.error(`\n${e}\n`);
+        }
     });
-};
+}
 
 main();
