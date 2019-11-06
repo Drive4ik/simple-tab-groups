10e3329a803964c09cdec000b6c0f451fb963b06
Drive4ik <drive4ik@gmail.com>
2019-11-06 12:35:46 +0200

    small fix for containers in backup

--- addon/src/options/manage-addon-backup.vue
+++ addon/src/options/manage-addon-backup.vue
@@ -131,7 +131,7 @@
                                 class="size-16 fill-context"
                                 />
                         </figure>
-                        <figure v-if="TEMPORARY_CONTAINER !== group.newTabContainer && data.containers && data.containers[group.newTabContainer]" class="image is-16x16 is-inline-block">
+                        <figure v-if="TEMPORARY_CONTAINER !== group.newTabContainer && data.containers && data.containers[group.newTabContainer] && data.containers[group.newTabContainer].iconUrl" class="image is-16x16 is-inline-block">
                             <img
                                 :src="data.containers[group.newTabContainer].iconUrl"
                                 :style="{fill: data.containers[group.newTabContainer].colorCode}"
