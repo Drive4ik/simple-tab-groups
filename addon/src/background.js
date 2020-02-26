ed862773ffbecc0de1ec5238f0ad4f5b31071776
Drive4ik <drive4ik@gmail.com>
2020-02-26 13:06:00 +0200

    add wait set session

--- addon/src/background.js
+++ addon/src/background.js
@@ -2802,7 +2802,7 @@ async function init() {
                         }
                     } else if (!tab.hidden) {
                         if (utils.isTabLoading(tab) || tab.url.startsWith('file:') || tab.lastAccessed > window.BG.startTime) {
-                            cache.setTabGroup(tab.id, win.groupId);
+                            await cache.setTabGroup(tab.id, win.groupId);
                             cache.applyTabSession(tab);
                             return;
                         }
