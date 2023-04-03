c21ede6018974b74bb9d6405e07ac89c00e29988
drive4ik <drive4ik+stg@protonmail.com>
2023-04-03 16:06:58 +0300

    [stg-plugin-create-new-group] refactor for manifest v3, bigfixes

--- plugins/stg-plugin-create-new-group/background.js
+++ plugins/stg-plugin-create-new-group/background.js
@@ -1,32 +1,38 @@
-(function() {
-    'use strict';
 
-    const STG_ID = 'simple-tab-groups@drive4ik',
-        STG_HOME_PAGE = 'https://addons.mozilla.org/firefox/addon/simple-tab-groups/';
+const STG_ID = 'simple-tab-groups@drive4ik',
+    STG_HOME_PAGE = 'https://addons.mozilla.org/firefox/addon/simple-tab-groups/';
 
-    browser.notifications.onClicked.addListener(function() {
-        browser.tabs.create({
-            url: STG_HOME_PAGE,
-        });
+browser.notifications.onClicked.addListener(() => {
+    browser.tabs.create({
+        url: STG_HOME_PAGE,
     });
+});
 
-    browser.browserAction.onClicked.addListener(function() {
-        browser.runtime.sendMessage(STG_ID, {
-            action: 'add-new-group',
-        }, function(responce) {
-            if (responce && responce.ok) {
-                browser.runtime.sendMessage(STG_ID, {
-                    action: 'load-last-group',
-                });
-            } else {
-                browser.notifications.create({
-                    type: 'basic',
-                    iconUrl: '/icons/icon.svg',
-                    title: browser.i18n.getMessage('extensionName'),
-                    message: browser.i18n.getMessage('needInstallSTGExtension'),
-                });
-            }
+browser.action.onClicked.addListener(async () => {
+    const responce = await browser.runtime.sendMessage(STG_ID, {
+        action: 'add-new-group',
+    }).catch(() => {});
+
+    if (responce) {
+        if (responce.ok) {
+            browser.runtime.sendMessage(STG_ID, {
+                action: 'load-last-group',
+            });
+        } else {
+            browser.notifications.create({
+                type: 'basic',
+                iconUrl: '/icons/icon.svg',
+                title: browser.i18n.getMessage('extensionName'),
+                message: responce.error,
+            });
+        }
+    } else {
+        browser.notifications.create({
+            type: 'basic',
+            iconUrl: '/icons/icon.svg',
+            title: browser.i18n.getMessage('extensionName'),
+            message: browser.i18n.getMessage('needInstallSTGExtension'),
         });
-    });
+    }
 
-})()
+});
